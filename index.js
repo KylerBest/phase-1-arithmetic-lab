082a3d68f890b2e04bf4685e9c14e8a2de426127
Kyler <kyler.b@live.com>
2021-11-01 13:23:37 -0500

    commit tree 45ea29452c627e9fbfefef57b9d1f5e096b55b41

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 24;
+const num4 = 5;
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 10 + 1);
+let mod = num3 % num4;
+let max = Math.max(5, 7, 17, 13, 20);
